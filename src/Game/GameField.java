78e266b809920b074b9b7158109cb1317a6f1879
hhoanguet <hhoanguet@gmail.com>
2019-11-17 19:57:25 +0700

    nearly shoot

--- src/Game/GameField.java
+++ src/Game/GameField.java
@@ -1,9 +1,11 @@
 package Game;
 
+import Bullet.Bullet;
 import Enemy.Enemy;
 import Enemy.EnemyGenerator;
 import Initialization.Background;
 import Tower.Tower;
+import com.sun.javafx.geom.Vec2d;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
@@ -11,7 +13,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
+import Tower.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -20,17 +22,20 @@ public class GameField extends AnimationTimer {
     protected GraphicsContext gc;
     private final int startingLevel = 1;
     protected AnchorPane root = new AnchorPane();
+
     public GameField(GraphicsContext gc, AnchorPane root) {
         this.gc = gc;
         this.root = root;
         road = new Background(gc);
         restartGame();
     }
+
     private Background road;
     private static Queue<Enemy> enemyQueue = new LinkedList<Enemy>();
-    public  static Queue<Enemy> activeEnemyQueue = new LinkedList<Enemy>();
+    public static Queue<Enemy> activeEnemyQueue = new LinkedList<Enemy>();
     private ArrayList<Enemy> enemyList;
     private static ArrayList<Tower> towerList = new ArrayList<Tower>();
+    private static ArrayList<Bullet> bulletList = new ArrayList<Bullet>();
     private final int enemySpawnDelay = 20;
     private static boolean waveIsInProgress = true;
     static long tickCount = 0;
@@ -47,10 +52,9 @@ public class GameField extends AnimationTimer {
     private Text textCash = new Text("");
     private Text textGameOver = new Text("GAME OVER");
 
-
-    public void addEnemiesToActiveEnemyQueue(){
+    public void addEnemiesToActiveEnemyQueue() {
         tickCount++;
-        if(tickCount > enemySpawnDelay){
+        if (tickCount > enemySpawnDelay) {
             activeEnemyQueue.add(enemyQueue.poll());
             tickCount = 0;
         }
@@ -65,10 +69,13 @@ public class GameField extends AnimationTimer {
         activeEnemyQueue = new LinkedList<Enemy>();
         createEnemyQueueForLevel();
         gameOver = false;
+        towerList.add(new NormalTower(gc,new Vec2d(0,0)));
+        towerList.add(new SniperTower(gc,new Vec2d(7*32,8*32)));
+        towerList.add(new MachineGunTower(gc, new Vec2d(4*32,4*32)));
     }
 
 
-    public void createEnemyQueueForLevel(){
+    public void createEnemyQueueForLevel() {
         generator = new EnemyGenerator(currentLevel - 1);
         generator.createEnemyQueue(gc);
         generator.RandomizeEnemyQueue();
@@ -77,19 +84,19 @@ public class GameField extends AnimationTimer {
         activeEnemyQueue.add(enemyQueue.poll());
     }
 
-    public void updateEnemies(){
+    public void updateEnemies() {
         ArrayList<Enemy> enemiesToRemove = new ArrayList<Enemy>();
         //for each enemy list, update their movement if they are alive
-        for(Enemy s : activeEnemyQueue){
+        for (Enemy s : activeEnemyQueue) {
             //only living enemies can move!
-            if(s.isAlive())
+            if (s.isAlive())
                 s.update(road.getMap());
-            else{
+            else {
                 Player.getPlayer().addCredits(s.getPrize());
                 enemiesToRemove.add(s);
             }
 
-            if(s.isAtEndPoint()){
+            if (s.isAtEndPoint()) {
                 Player.getPlayer().decreaseLife();
                 root.getChildren().remove(s.healthBar());
                 enemiesToRemove.add(s);
@@ -97,11 +104,11 @@ public class GameField extends AnimationTimer {
         }
 
         //remove all the dead critters and the critters that have arrived at the exit
-        for(Enemy s : enemiesToRemove){
+        for (Enemy s : enemiesToRemove) {
             activeEnemyQueue.remove(s);
         }
 
-        if(enemyQueue.size()==0 && activeEnemyQueue.size() == 0){
+        if (enemyQueue.size() == 0 && activeEnemyQueue.size() == 0) {
 //            waveIsInProgress = false;
             currentLevel++;
             createEnemyQueueForLevel();
@@ -110,24 +117,25 @@ public class GameField extends AnimationTimer {
 
     public void update() {
 
-        if(waveIsInProgress){
-            if(enemyQueue.size()!=0){
+        if (waveIsInProgress) {
+            if (enemyQueue.size() != 0) {
                 addEnemiesToActiveEnemyQueue();
-            } else if(enemyQueue.size() == 0 && activeEnemyQueue.size() == 0){
+            } else if (enemyQueue.size() == 0 && activeEnemyQueue.size() == 0) {
                 currentLevel++;
             }
-            if(!gameOver){
+            if (!gameOver) {
                 updateEnemies();
             }
         }
 
-        if(Player.getPlayer().getLives() <= 0){
+        if (Player.getPlayer().getLives() <= 0) {
             System.out.println(Player.getPlayer().getLives() + " het mau cmnr");
             gameOver = true;
         }
 
     }
-    public void drawText(Text text, int size, double x,  double y){
+
+    public void drawText(Text text, int size, double x, double y) {
         Font font = Font.font("Verdana", FontWeight.NORMAL, size);
         text.setFill(Color.BLACK);
         text.setFont(font);
@@ -137,23 +145,69 @@ public class GameField extends AnimationTimer {
         root.getChildren().add(text);
     }
 
-    public void attackEnemies() {
+    public void targetEnemies() {
         for(Tower t: towerList) {
-            if(t.getTargetEnemy()!= null && t.canAttack()) {
-
+            t.setTargetEnemy(null);
+            //
+            //  ENEMY THIEU HAM ISVISIBLE
+            //
+            for(Enemy e : activeEnemyQueue) {
+                if(e.isAlive() ) {
+                    // Calculate distance
+                    double xDist = Math.abs(e.getPosition().x - t.getPosition().x);
+                    double yDist = Math.abs(e.getPosition().x - t.getPosition().y);
+                    double dist = Math.sqrt((xDist*xDist) + (yDist*yDist));
+                    // ENEMY THIEU HAM getEnemyTravelDistanceMaximum
+                    if(dist < t.getRange()) {
+                        t.setTargetEnemy(e);
+                    }
+                }
+            }
+            t.setEnemyTravelDistanceMaximum(0);
+        }
+    }
+    public void attackEnemies() {
+        for (Tower t : towerList) {
+            if (t.getTargetEnemy() != null && t.canAttack()) {
+                attackEnemy(t);
+                t.setTimeOfLastAttack(System.currentTimeMillis());
             }
         }
     }
 
     public void attackEnemy(Tower source) {
-
+        Bullet bullet = new Bullet(gc, new Vec2d(source.getPosition().x,source.getPosition().y), new Vec2d(source.getTargetEnemy().getPosition().x,source.getTargetEnemy().getPosition().y),
+                                    source.getPower(),source.getTargetEnemy());
+        bulletList.add(bullet);
     }
+
+    public void reloadTowers() {
+        for(Tower t : towerList) {
+            t.setTimeOfLastAttack(0);
+        }
+    }
+
+    public Tower getNearestTower( int x , int y ) {
+        double distanceApproximation = 100;
+        Tower neareastTower = null;
+        for ( Tower t : towerList) {
+            if ( Math.abs(t.getPosition().x - x) + Math.abs(t.getPosition().y -y) < distanceApproximation ) {
+                neareastTower = t;
+                distanceApproximation = Math.abs(t.getPosition().x - x) + Math.abs(t.getPosition().y - y) ;
+            }
+        }
+        return neareastTower;
+    }
+
     public void draw() {
         road.draw();
-        if(waveIsInProgress) {
+        if (waveIsInProgress) {
             for (Enemy s : activeEnemyQueue) {
                 s.draw();
                 s.drawEnemyHealth(root);
+            }
+            for (Tower t : towerList) {
+                t.draw();
             }
         }
 
@@ -163,22 +217,22 @@ public class GameField extends AnimationTimer {
         textLives.setText(lives);
         textCash.setText(cash);
         textLevel.setText(level);
-        drawText(textLives,20, 100, 300);
-        drawText(textCash,20, 300, 300);
-        drawText(textLevel,20, 500, 300);
+        drawText(textLives, 20, 100, 300);
+        drawText(textCash, 20, 300, 300);
+        drawText(textLevel, 20, 500, 300);
 
-        if(gameOver){
+        if (gameOver) {
             root.getChildren().remove(textCash);
             root.getChildren().remove(textLevel);
             root.getChildren().remove(textLives);
-            drawText(textGameOver,40, 200, 300);
+            drawText(textGameOver, 40, 200, 300);
         }
     }
+
 
     @Override
     public void handle(long now) {
         update();
         draw();
     }
-
 }
---
+++ src/Tower/SniperTower.java
@@ -0,0 +1,21 @@
+package Tower;
+import Initialization.ImageProcessing;
+import com.sun.javafx.geom.Vec2d;
+import javafx.scene.canvas.GraphicsContext;
+public class SniperTower extends Tower{
+    public SniperTower(GraphicsContext gc, Vec2d pos) {
+        super(pos);
+        this.gc = gc;
+        this.buyingCost = newBuyingCost;
+        this.refundValue = newRefundValue;
+        this.reloadTime = newReloadTime;
+        this.range = newRange;
+        this.power = newPower;
+        this.image = ImageProcessing.splits(19,10);
+    }
+    private static int newBuyingCost = 300;
+    private static int newRefundValue = 250;
+    private static int newReloadTime = 7;
+    private static double newRange = 300;
+    private static int newPower = 20;
+}
