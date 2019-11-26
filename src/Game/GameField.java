9373905d8a31cf156830d578f4702b017206eeff
hhoanguet <hhoanguet@gmail.com>
2019-11-26 18:54:11 +0700

    adds tower's button ver 1.0

--- src/Game/GameField.java
+++ src/Game/GameField.java
@@ -4,9 +4,13 @@ import Enemy.Enemy;
 import Enemy.EnemyGenerator;
 import Initialization.Background;
 import Initialization.Config;
+import Initialization.ImageProcessing;
 import Scene.SceneManager;
+import Tower.MachineGunTower;
+import Tower.NormalTower;
 import Tower.SniperTower;
 import Tower.Tower;
+import Tower.TowerMenu;
 import com.sun.javafx.geom.Vec2d;
 import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
@@ -15,6 +19,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -66,7 +71,11 @@ public class GameField extends AnimationTimer {
     private String lives;
     private String cash;
     EnemyGenerator generator;
-
+    private TowerMenu nT, sT , mT;
+    private VBox towerMenu = new VBox();
+    private ImageView tower = null;
+    private double tempX, tempY;
+    private String whichTower;
     Font font = Font.font("Verdana", FontWeight.NORMAL, 20);
     private Text textLevel = new Text("");
     private Text textLives = new Text("");
@@ -80,6 +89,84 @@ public class GameField extends AnimationTimer {
             activeEnemyQueue.add(enemyQueue.poll());
             tickCount = 0;
         }
+    }
+
+    public void addListener(Stage stage) {
+        // Listen to the click of the Tower Button
+        nT.addEventFilter(MouseEvent.MOUSE_CLICKED, e ->{
+            if(true) {
+                if (tower == null) {
+                    tower = new ImageView(ImageProcessing.splits(19,8));
+                    whichTower = "Normal";
+                    tower.setFitWidth(32);
+                    tower.setFitHeight(32);
+                    tower.setX(stage.getWidth() - (nT.getPrefWidth()/2));
+                    tower.setY(0);
+                    tower.setPreserveRatio(true);
+                    root.getChildren().add(tower);
+                }
+            }
+        } );
+        sT.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+            if(true) {
+                if(tower == null) {
+                    tower = new ImageView(ImageProcessing.splits(19,10));
+                    whichTower = "Sniper";
+                    tower.setFitWidth(32);
+                    tower.setFitHeight(32);
+                    tower.setX(stage.getWidth() - (sT.getPrefWidth()/2));
+                    tower.setY(20);
+                    tower.setPreserveRatio(true);
+                    root.getChildren().add(tower);
+                }
+            }
+        });
+
+        mT.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+            if(true) {
+                if(tower == null) {
+                    tower = new ImageView(ImageProcessing.splits(20,10));
+                    whichTower = "Sniper";
+                    tower.setFitWidth(32);
+                    tower.setFitHeight(32);
+                    tower.setX(stage.getWidth() - (mT.getPrefWidth()/2));
+                    tower.setY(50);
+                    tower.setPreserveRatio(true);
+                    root.getChildren().add(tower);
+                }
+            }
+        });
+
+
+
+        // MOVE THE TOWER AROUND ON THE SCREEN
+
+        stage.getScene().addEventHandler(MouseEvent.MOUSE_MOVED, e -> {
+            if(tower != null) {
+                tower.setVisible(true);
+                tempX = e.getX() - (tower.getFitWidth() / 2);
+                tempY = e.getY() - (tower.getFitHeight() / 2);
+                tower.setX(e.getX() - (tower.getFitWidth() / 2));
+                tower.setY(e.getY() - (tower.getFitHeight() / 2));
+
+            }
+        });
+
+        // PLACES THE TOWER ON THE CLICKED POSITION
+        stage.getScene().addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            if(tower != null) {
+                if(whichTower.equals("Normal")) {
+                    towerList.add(new NormalTower(gc,new Vec2d(tempX,tempY),activeEnemyQueue));
+                    whichTower = "";
+                }
+
+                if(whichTower.equals("Sniper")) {
+                    towerList.add(new SniperTower(gc,new Vec2d(tempX,tempY),activeEnemyQueue));
+                    whichTower = "";
+                }
+            }
+        });
+
     }
 
 
@@ -103,14 +190,27 @@ public class GameField extends AnimationTimer {
         activeEnemyQueue = new LinkedList<Enemy>();
         createEnemyQueueForLevel();
         gameOver = false;
+        createTowerMenu();
         towerList.add(new SniperTower(gc,new Vec2d(15*32,10*32),activeEnemyQueue));
-//        towerList.add(new MachineGunTower(gc,new Vec2d(8*32,8*32),activeEnemyQueue));
-//        towerList.add(new NormalTower(gc, new Vec2d(4*32,4*32),activeEnemyQueue));
-//        towerList.add(new MachineGunTower(gc,new Vec2d(13*32,4*32),activeEnemyQueue));
-//        towerList.add(new NormalTower(gc,new Vec2d(3*32,11*32),activeEnemyQueue));
+        towerList.add(new MachineGunTower(gc,new Vec2d(8*32,8*32),activeEnemyQueue));
+        towerList.add(new NormalTower(gc, new Vec2d(4*32,4*32),activeEnemyQueue));
+        towerList.add(new MachineGunTower(gc,new Vec2d(13*32,4*32),activeEnemyQueue));
+        towerList.add(new NormalTower(gc,new Vec2d(3*32,11*32),activeEnemyQueue));
         bgAudio.playCycle(36);
+        addListener(stage);
     }
 
+    public void createTowerMenu() {
+        nT = new TowerMenu("Normal $20 ");
+        sT = new TowerMenu("Sniper $30 ");
+        mT = new TowerMenu("MachineGun $50 ");
+        towerMenu.getChildren().add(nT);
+        towerMenu.getChildren().add(sT);
+        towerMenu.getChildren().add(mT);
+        towerMenu.setLayoutX(stage.getWidth() - mT.getPrefWidth());
+        towerMenu.setLayoutY(0);
+        root.getChildren().add(towerMenu);
+    }
 
     public void createEnemyQueueForLevel() {
         generator = new EnemyGenerator(currentLevel - 1);
@@ -118,7 +218,7 @@ public class GameField extends AnimationTimer {
         generator.RandomizeEnemyQueue();
         enemyQueue = generator.getCritterQueue();
 //        activeEnemyQueue = new LinkedList<Enemy>();
-      //  activeEnemyQueue = new LinkedList<Enemy>();  HUHU
+      //  activeEnemyQueue = new LinkedList<Enemy>();
         activeEnemyQueue.add(enemyQueue.poll());
     }
 
@@ -151,12 +251,6 @@ public class GameField extends AnimationTimer {
 //            waveIsInProgress = false;
             currentLevel++;
             createEnemyQueueForLevel();
-        }
-    }
-
-    public void reloadTowers() {
-        for(Tower t : towerList) {
-            t.setLastAttackTime(0);
         }
     }
 
---
+++ src/Tower/TowerMenu.java
@@ -0,0 +1,11 @@
+package Tower;
+
+import javafx.scene.control.Button;
+
+public class TowerMenu extends Button {
+    public TowerMenu(String text) {
+        this.setText(text);
+        this.setVisible(true);
+        this.setPrefWidth(120);
+    }
+}
